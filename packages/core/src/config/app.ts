c2ae4e0a6234b13b9f758adbc9dffd8456172599
yunpengliu <liuyp@sugon.com>
2026-02-05 17:22:22 +0800

    release v0.3.5

--- packages/core/src/config/app.ts
+++ packages/core/src/config/app.ts
@@ -71,7 +71,7 @@ export const APP_THEMES: AppTheme[] = [
     }
 ];
 
-export const APP_VERSION = '0.3.4';
+export const APP_VERSION = '0.3.5';
 
 const getContentUrl = (path: string): string => {
     const baseUrl = import.meta.env.BASE_URL;
